55b9d458bf39479edaa206d6958e17aa6af5ba13
gbesset <guillaume.besset.pro@gmail.com>
2023-02-07 12:03:42 +0100

    feat: change mapping type & add unregister

--- voting.sol
+++ voting.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.18;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
-    //QUESTION mettre les definitions qq part et importer !
+    //QUESTION mettre les deinitions qq pat et importer !
     struct Voter {
         bool isRegistered;
         bool hasVoted;
@@ -27,7 +27,7 @@ contract Voting is Ownable {
     }
 
     uint256 private winningProposalId; //private ou pas ? sert a rien..
-    mapping(address => bool) private whitelist;
+    mapping(address => Voter) private whitelist;
 
     event VoterRegistered(address voterAddress);
     event WorkflowStatusChange(
@@ -37,6 +37,8 @@ contract Voting is Ownable {
     event ProposalRegistered(uint256 proposalId);
     event Voted(address voter, uint256 proposalId);
 
+    event VoterUnRegistered(address voterAddress);
+
     constructor() {
         //TODO L’administrateur est celui qui va déployer le smart contract.
     }
@@ -45,13 +47,49 @@ contract Voting is Ownable {
         return winningProposalId;
     }
 
+    modifier isRegistered(address _address) {
+        require(whitelist[msg.sender].isRegistered, "You are not registered");
+        require(whitelist[_address].isRegistered, "the user is not registered");
+        _;
+    }
+
     function authorize(address _address) public onlyOwner {
-        require(!whitelist[_address], "Address is already white listed");
-        whitelist[_address] = true;
+        require(
+            !whitelist[_address].isRegistered,
+            "Address is already registered"
+        );
+        whitelist[_address].isRegistered = true;
         emit VoterRegistered(_address);
     }
 
+    function unregister(address _address) public onlyOwner {
+        require(whitelist[_address].isRegistered, "Address is not registered");
+        whitelist[_address].isRegistered = false;
+        emit VoterUnRegistered(_address);
+    }
+
     function isWhitelisted(address _address) public view returns (bool) {
-        return whitelist[_address];
+        //Everybody can know if someone is whitelisted
+        return whitelist[_address].isRegistered;
+    }
+
+    function hasVoted(address _address)
+        public
+        view
+        isRegistered(_address)
+        returns (bool)
+    {
+        //only whitelisted people  can access that data on registered user
+        return whitelist[_address].hasVoted;
+    }
+
+    function votedForProposalId(address _address)
+        public
+        view
+        isRegistered(_address)
+        returns (uint256)
+    {
+        //only whitelisted people  can access that data on registered user
+        return whitelist[_address].votedProposalId;
     }
 }
