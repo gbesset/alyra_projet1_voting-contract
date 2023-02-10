77df870cb976f579080d56af39604d5a30473350
gbesset <guillaume.besset.pro@gmail.com>
2023-02-10 09:13:55 +0100

    Clean code getters

--- voting.sol
+++ voting.sol
@@ -30,7 +30,7 @@ contract Voting is Ownable {
     WorkflowStatus public voteStatus;
     // private because electors are registrerd on whitelist. unauthorized persons cannot see the whitelist. getters are protected by modifiers
     mapping(address => Voter) private whitelist;
-    Proposal[] public proposals;
+    Proposal[] private proposals;
 
     event VoterRegistered(address voterAddress);
     event WorkflowStatusChange(
@@ -90,7 +90,49 @@ contract Voting is Ownable {
 
     /* 
 *******************************
-        Whitelist management
+        getters
+*******************************
+*/
+    function getProposal(uint256 _id)
+        external
+        view
+        onlyRegistered
+        returns (Proposal memory)
+    {
+        return proposals[_id];
+    }
+
+    function getWinner()
+        external
+        view
+        checkWorkflowStatus(WorkflowStatus.VotesTallied)
+        returns (uint256)
+    {
+        return winningProposalId;
+    }
+
+    function getWinnerDetails()
+        external
+        view
+        checkWorkflowStatus(WorkflowStatus.VotesTallied)
+        returns (Proposal memory)
+    {
+        return proposals[winningProposalId];
+    }
+
+    function getVoterDetails(address _address)
+        external
+        view
+        onlyRegistered
+        onlyRegisteredAddress(_address)
+        returns (Voter memory)
+    {
+        return whitelist[_address];
+    }
+
+    /* 
+*******************************
+        Voters management
 *******************************
 */
 
@@ -115,30 +157,6 @@ contract Voting is Ownable {
         require(whitelist[_address].isRegistered, "Address is not registered");
         whitelist[_address].isRegistered = false;
         emit VoterUnRegistered(_address);
-    }
-
-    function isWhitelisted(address _address) external view returns (bool) {
-        return whitelist[_address].isRegistered;
-    }
-
-    function hasVoted(address _address)
-        external
-        view
-        onlyRegistered
-        onlyRegisteredAddress(_address)
-        returns (bool)
-    {
-        return whitelist[_address].hasVoted;
-    }
-
-    function votedForProposalId(address _address)
-        external
-        view
-        onlyRegistered
-        onlyRegisteredAddress(_address)
-        returns (uint256)
-    {
-        return whitelist[_address].votedProposalId;
     }
 
     /*
@@ -251,6 +269,10 @@ Feature
         onlyRegistered
         checkWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted)
     {
+        require(
+            keccak256(abi.encode(_proposal)) != keccak256(abi.encode("")),
+            "Proposal can't be empty"
+        );
         proposals.push(Proposal(_proposal, 0));
         emit ProposalRegistered(proposals.length - 1); // we count proposal 0, 1, 2 to make it easier
     }
@@ -285,23 +307,5 @@ Feature
         }
         winningProposalId = winner;
         emit winningProposal(winningProposalId);
-    }
-
-    function getWinner()
-        external
-        view
-        checkWorkflowStatus(WorkflowStatus.VotesTallied)
-        returns (uint256)
-    {
-        return winningProposalId;
-    }
-
-    function getWinnerDetails()
-        external
-        view
-        checkWorkflowStatus(WorkflowStatus.VotesTallied)
-        returns (Proposal memory)
-    {
-        return proposals[winningProposalId];
     }
 }
--- voting_session.sol
+++ voting_session.sol
@@ -41,7 +41,7 @@ contract Voting is Ownable {
     //Passé beaucoup de temps a me casser les dents pour mettre ma whiteList et Proposals dans ma struc en vain....
     //si meilleur idée que ca (héritage sans doute) je suis preneur. tkx :)
     mapping(uint256 => mapping(address => Voter)) voteSessionWhitelist;
-    mapping(uint256 => Proposal[]) public voteSessionProposals;
+    mapping(uint256 => Proposal[]) voteSessionProposals;
 
     event VoteSessionCreated(string _name);
     event VoterRegistered(address voterAddress);
@@ -99,6 +99,56 @@ contract Voting is Ownable {
 
     /* 
 *******************************
+        getters
+*******************************
+*/
+    function getProposal(uint256 _id)
+        external
+        view
+        onlyRegistered
+        returns (Proposal memory)
+    {
+        return voteSessionProposals[sessionId][_id];
+    }
+
+    //public to have one called by the contract and by people
+    function getWinner()
+        public
+        view
+        checkWorkflowStatus(WorkflowStatus.VotesTallied)
+        returns (uint256)
+    {
+        return voteSession[sessionId].winningProposalId;
+    }
+
+    //external because only people
+    function getWinnerDetails()
+        external
+        view
+        checkWorkflowStatus(WorkflowStatus.VotesTallied)
+        returns (string memory)
+    {
+        string memory message = string.concat(
+            "SessionId: ",
+            Strings.toString(sessionId),
+            "  Resultat: ",
+            voteSessionProposals[sessionId][getWinner() - 1].description
+        ); //-1 to get the good one
+        return message;
+    }
+
+    function getVoterDetails(address _address)
+        external
+        view
+        onlyRegistered
+        onlyRegisteredAddress(_address)
+        returns (Voter memory)
+    {
+        return voteSessionWhitelist[sessionId][_address];
+    }
+
+    /* 
+*******************************
         voteSession management
 *******************************
 */
@@ -145,30 +195,6 @@ contract Voting is Ownable {
         voteSessionWhitelist[sessionId][_address].isRegistered = false;
         voteSession[sessionId].nbElector--;
         emit VoterUnRegistered(_address);
-    }
-
-    function isWhitelisted(address _address) external view returns (bool) {
-        return voteSessionWhitelist[sessionId][_address].isRegistered;
-    }
-
-    function hasVoted(address _address)
-        external
-        view
-        onlyRegistered
-        onlyRegisteredAddress(_address)
-        returns (bool)
-    {
-        return voteSessionWhitelist[sessionId][_address].hasVoted;
-    }
-
-    function votedForProposalId(address _address)
-        external
-        view
-        onlyRegistered
-        onlyRegisteredAddress(_address)
-        returns (uint256)
-    {
-        return voteSessionWhitelist[sessionId][_address].votedProposalId;
     }
 
     /*
@@ -323,6 +349,10 @@ Feature
         onlyRegistered
         checkWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted)
     {
+        require(
+            keccak256(abi.encode(_proposal)) != keccak256(abi.encode("")),
+            "Proposal can't be empty"
+        );
         voteSessionProposals[sessionId].push(Proposal(_proposal, 0));
         emit ProposalRegistered(voteSessionProposals[sessionId].length); //this time we count 1, 2, 3
     }
@@ -366,31 +396,5 @@ Feature
         }
         voteSession[sessionId].winningProposalId = winner + 1; //we have to +1 to get the good one
         emit winningProposal(voteSession[sessionId].winningProposalId);
-    }
-
-    //public to have one called by the contract and by people
-    function getWinner()
-        public
-        view
-        checkWorkflowStatus(WorkflowStatus.VotesTallied)
-        returns (uint256)
-    {
-        return voteSession[sessionId].winningProposalId;
-    }
-
-    //external because only people
-    function getWinnerDetails()
-        external
-        view
-        checkWorkflowStatus(WorkflowStatus.VotesTallied)
-        returns (string memory)
-    {
-        string memory message = string.concat(
-            "SessionId: ",
-            Strings.toString(sessionId),
-            "  Resultat: ",
-            voteSessionProposals[sessionId][getWinner() - 1].description
-        ); //-1 to get the good one
-        return message;
     }
 }
